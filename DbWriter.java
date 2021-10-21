5f1b4f5f0963a303192f8e9297d7c84ec5ab3ac2
Gimzar <56764583+Gimzar@users.noreply.github.com>
2021-10-21 07:50:54 +0300

    Update DbWriter.java

--- DbWriter.java
+++ DbWriter.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DbWriter {
 
-  /*  public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -15,5 +15,5 @@ public class DbWriter {
         }
         Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/stszpua_db","testuser", "testparol");
         connection.close();
-    }*/
+    }
 }
